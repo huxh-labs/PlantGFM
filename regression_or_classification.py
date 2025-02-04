15db9fb35c8df5e771ef3f4e8f4cf77676ee0c11
hu-lab-PlantGLM <lichanghao6353@163.com>
2025-02-04 14:52:56 +0800

    test

--- regression_or_classification.py
+++ regression_or_classification.py
@@ -6,8 +6,8 @@ import argparse
 from datasets import Dataset, DatasetDict
 from sklearn.metrics import r2_score, accuracy_score, f1_score, matthews_corrcoef
 from transformers import Trainer, TrainingArguments, PreTrainedTokenizerFast
-from plantglm.modeling_plantglm import PlantGLMForSequenceClassification
-from plantglm.configuration_plantglm import PlantGLMConfig
+from plantgfm.modeling_plantgfm import PlantGFMForSequenceClassification
+from plantgfm.configuration_plantgfm import PlantGFMConfig
 
 # Disable parallelism in tokenizers to prevent warnings
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -20,7 +20,7 @@ def tokenize_function(examples, tokenizer, max_length):
         truncation=True, 
         max_length=max_length, 
     )
-
+    
 # Function to compute evaluation metrics for regression task
 def compute_metrics_for_regression_task(eval_pred):
     predictions, labels = eval_pred
@@ -34,28 +34,8 @@ def compute_metrics_for_classification_task(eval_pred):
     acc = accuracy_score(labels, pred_labels)
     return {"accuracy": acc}
 
-# Sigmoid function for segmentation
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
-
-# Convert logits to labels for segmentation task
-def logits_to_labels(x):
-    pred_labels = np.zeros_like(x)
-    pred_labels[x >= 0.50] = 1
-    return pred_labels
-
-# Function to compute evaluation metrics for segmentation task
-def compute_metrics_for_segmentation_task(eval_pred):
-    logits, labels = eval_pred
-    if isinstance(logits, tuple):
-        logits = logits[0]
-    pred_labels = logits_to_labels(sigmoid(logits))
-    return {
-        "gene_mcc": matthews_corrcoef(labels.ravel(), pred_labels.ravel())
-    }
-
 # Main function to train and test the model
-def train_and_test(data_name, output_dir, model_name_or_path, tokenizer_path, max_length, batch_size, epochs, learning_rate, logging_strategy, evaluation_strategy, save_strategy, save_total_limit, weight_decay, metric_for_best_model, task_type):
+def train_and_test(data_name, output_dir, model_name_or_path, tokenizer_path, max_length, batch_size, epochs, learning_rate, logging_strategy, evaluation_strategy, save_strategy, save_total_limit, weight_decay, task_type):
     # Load the data
     train_data = pd.read_csv(f'{data_name}/train.csv')
     val_data = pd.read_csv(f'{data_name}/val.csv')
@@ -80,7 +60,7 @@ def train_and_test(data_name, output_dir, model_name_or_path, tokenizer_path, ma
     tokenized_datasets = raw_datasets.map(lambda examples: tokenize_function(examples, tokenizer, max_length), batched=True)
 
     # Load model and configure it for sequence classification
-    model = PlantGLMForSequenceClassification.from_pretrained(model_name_or_path, num_labels=1)
+    model = PlantGFMForSequenceClassification.from_pretrained(model_name_or_path, num_labels=1)
     model.config.pad_token_id = tokenizer.pad_token_id
 
     # Set up training arguments
@@ -96,21 +76,23 @@ def train_and_test(data_name, output_dir, model_name_or_path, tokenizer_path, ma
         save_safetensors=False,
         learning_rate=learning_rate,
         ddp_find_unused_parameters=False,
-        metric_for_best_model=metric_for_best_model,
         load_best_model_at_end=True,
         bf16=True,
         weight_decay=weight_decay,
     )
 
-    # Select the appropriate metrics function based on the task type
+    # Set metric_for_best_model based on task_type
     if task_type == 'regression':
         compute_metrics = compute_metrics_for_regression_task
+        metric_for_best_model = 'r2'
     elif task_type == 'classification':
         compute_metrics = compute_metrics_for_classification_task
-    elif task_type == 'segmentation':
-        compute_metrics = compute_metrics_for_segmentation_task
+        metric_for_best_model = 'accuracy'
     else:
-        raise ValueError("Invalid task type. Choose from 'regression', 'classification', or 'segmentation'.")
+        raise ValueError("Invalid task type. Choose from 'regression', 'classification'")
+
+    # Update training arguments with metric_for_best_model
+    training_args.metric_for_best_model = metric_for_best_model
 
     # Create the trainer
     trainer = Trainer(
@@ -148,10 +130,9 @@ def parse_args():
     parser.add_argument('--save_strategy', type=str, default='epoch', choices=['steps', 'epoch'], help="Strategy for saving checkpoints.")
     parser.add_argument('--save_total_limit', type=int, default=1, help="The maximum number of checkpoints to save.")
     parser.add_argument('--weight_decay', type=float, default=0.001, help="Weight decay for optimization.")
-    parser.add_argument('--metric_for_best_model', type=str, default='r2', help="Metric to determine the best model.")
     
-    # Task type (regression, classification, segmentation)
-    parser.add_argument('--task_type', type=str, required=True, choices=['regression', 'classification', 'segmentation'], help="Type of task (regression, classification, segmentation).")
+    # Task type (regression, classification)
+    parser.add_argument('--task_type', type=str, required=True, choices=['regression', 'classification'], help="Type of task (regression, classification).")
     
     return parser.parse_args()
 
@@ -174,6 +155,5 @@ if __name__ == "__main__":
         save_strategy=args.save_strategy,
         save_total_limit=args.save_total_limit,
         weight_decay=args.weight_decay,
-        metric_for_best_model=args.metric_for_best_model,
         task_type=args.task_type
     )
